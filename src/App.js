782451b8ef968549427665b372ccb78ccda349ac
baeian <ddoory1103@gmail.com>
2022-10-19 11:06:18 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="score-wrapper">
+					<div className="wrapper">
+						<p className="score-title">score</p>
+					</div>
+					<div className="wrapper">
+						<h1 className="score-value">13296</h1>
+					</div>
+				</div>
+				<table className="table">
+					<tbody>
+						<tr className="row">
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+						</tr>
+						<tr className="row">
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+						</tr>
+						<tr className="row">
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+						</tr>
+						<tr className="row">
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+							<td className="block"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
 
 export default App;
